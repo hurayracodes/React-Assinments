3bcf9f4e454d9005b1251cb66c59b941accaa48a
Abu Hurayra <abuhuraira3912l@gmail.com>
2025-10-07 12:55:48 +0500

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import FormHadling from './projects/formHadling'
+import FormHadling from './projects/FormHadling'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 // import App from './App.jsx'
 // import Counter from './projects/counter.jsx'
 // import ColorChange from './projects/ColorChange.jsx'
